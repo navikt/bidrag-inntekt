613d426cc61a6643ef540159e3c021236fa4debf
rinnan17 <magnus.rinnan@nav.no>
2023-07-10 15:05:27 +0200

    Controller, skattegrunnlag

--- src/test/kotlin/no/nav/bidrag/inntekt/controller/InntektControllerTest.kt
+++ src/test/kotlin/no/nav/bidrag/inntekt/controller/InntektControllerTest.kt
@@ -8,6 +8,7 @@ import no.nav.bidrag.inntekt.dto.TransformerInntekterRequestDto
 import no.nav.bidrag.inntekt.dto.TransformerInntekterResponseDto
 import no.nav.bidrag.inntekt.exception.RestExceptionHandler
 import no.nav.bidrag.inntekt.service.InntektService
+import no.nav.bidrag.inntekt.service.SkattegrunnlagService
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import org.junit.jupiter.api.Assertions.assertAll
 import org.junit.jupiter.api.Assertions.assertNotNull
@@ -27,8 +28,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders
 @ActiveProfiles(TEST_PROFILE)
 @SpringBootTest(classes = [BidragInntektTest::class], webEnvironment = WebEnvironment.RANDOM_PORT)
 @EnableMockOAuth2Server
-class InntektControllerTest(@Autowired val exceptionLogger: ExceptionLogger) {
-    private val inntektService: InntektService = InntektService()
+class InntektControllerTest(
+    @Autowired val exceptionLogger: ExceptionLogger,
+    @Autowired val skattegrunnlagService: SkattegrunnlagService,
+    ) {
+
+    private val inntektService: InntektService = InntektService(skattegrunnlagService)
     private val inntektController: InntektController = InntektController(inntektService)
     private val mockMvc: MockMvc = MockMvcBuilders.standaloneSetup(inntektController)
         .setControllerAdvice(RestExceptionHandler(exceptionLogger))
