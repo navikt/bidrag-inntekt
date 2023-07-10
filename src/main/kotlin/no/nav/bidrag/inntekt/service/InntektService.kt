3800a8898daf9df51d64d8d39491c2ba8c93eeb3
Lars Otto Haugen <lars.otto.haugen@nav.no>
2023-07-10 11:04:01 +0200

    Endret json-formattering på dato og forenklet kode

--- src/main/kotlin/no/nav/bidrag/inntekt/service/InntektService.kt
+++ src/main/kotlin/no/nav/bidrag/inntekt/service/InntektService.kt
@@ -13,12 +13,11 @@ import no.nav.bidrag.inntekt.dto.TransformerInntekterRequestDto
 import no.nav.bidrag.inntekt.dto.TransformerInntekterResponseDto
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 
 @Service
 class InntektService {
-
-    val objectMapper: ObjectMapper = jacksonObjectMapper().registerKotlinModule().registerModule(JavaTimeModule())
 
     fun transformerInntekter(transformerInntekterRequestDto: TransformerInntekterRequestDto): TransformerInntekterResponseDto {
         return TransformerInntekterResponseDto(inntektListe = konverterAinntekt(transformerInntekterRequestDto.ainntektListe))
@@ -26,7 +25,12 @@ class InntektService {
 
     // Summerer, grupperer og transformerer ainntekter
     private fun konverterAinntekt(ainntektListeInn: List<AinntektDto>): List<Inntekt> {
-        val ainntektMap = summerInntekterNy(ainntektListeInn)
+        val objectMapper: ObjectMapper = jacksonObjectMapper()
+        objectMapper.registerKotlinModule()
+        objectMapper.registerModule(JavaTimeModule())
+        objectMapper.dateFormat = SimpleDateFormat("yyyy-MM-dd")
+
+        val ainntektMap = summerInntekter(ainntektListeInn)
 
         val ainntektListeUt = mutableListOf<Inntekt>()
         val dagensDato = LocalDate.now()
@@ -39,8 +43,6 @@ class InntektService {
                         inntektType = InntektType.AINNTEKT,
                         datoFra = LocalDate.parse(it.key + "-01-01"),
                         datoTil = if (it.key.toInt() == dagensAar) dagensDato else LocalDate.parse(it.key + "-01-01").plusYears(1),
-//                        sumInntekt = it.value,
-//                        inntektPostListe = ObjectMapper().createObjectNode()
                         sumInntekt = it.value.sumInntekt,
                         inntektPostListe = objectMapper.valueToTree(it.value.inntektPostListe)
                     )
@@ -51,10 +53,7 @@ class InntektService {
                         inntektType = if (it.key == KEY_3MND) InntektType.AINNTEKT_BEREGNET_3MND else InntektType.AINNTEKT_BEREGNET_12MND,
                         datoFra = if (it.key == KEY_3MND) dagensDato.minusMonths(3) else dagensDato.minusYears(1),
                         datoTil = dagensDato,
-//                        sumInntekt = it.value,
-//                        inntektPostListe = ObjectMapper().createObjectNode()
                         sumInntekt = it.value.sumInntekt,
-//                        inntektPostListe = ObjectMapper().valueToTree(it.value.inntektPostListe)
                         inntektPostListe = objectMapper.valueToTree(it.value.inntektPostListe)
                     )
                 )
@@ -65,60 +64,33 @@ class InntektService {
     }
 
     // Summerer og grupperer ainntekter
-    private fun summerInntekter(ainntektListeInn: List<AinntektDto>): Map<String, BigDecimal> {
-        val ainntektMap = mutableMapOf<String, BigDecimal>()
+    private fun summerInntekter(ainntektListeInn: List<AinntektDto>): Map<String, InntektSumPost> {
+        val ainntektMap = mutableMapOf<String, InntektSumPost>()
         ainntektListeInn.forEach { ainntekt ->
             ainntekt.ainntektspostListe.forEach {
                 val aar =
                     if (it.opptjeningsperiodeFra != null) it.opptjeningsperiodeFra!!.year.toString() else it.utbetalingsperiode!!.substring(0, 4)
-                val summertAinntekt = ainntektMap.getOrDefault(aar, BigDecimal.ZERO).add(it.belop)
-                ainntektMap[aar] = summertAinntekt
+                akkumulerPost(ainntektMap, it, aar)
 
                 if (innenforAntallMnd(it, 3)) {
-                    val summertAinntekt3Mnd = ainntektMap.getOrDefault(KEY_3MND, BigDecimal.ZERO).add(it.belop)
-                    ainntektMap[KEY_3MND] = summertAinntekt3Mnd
+                    akkumulerPost(ainntektMap, it, KEY_3MND)
                 }
 
                 if (innenforAntallMnd(it, 12)) {
-                    val summertAinntekt12Mnd = ainntektMap.getOrDefault(KEY_12MND, BigDecimal.ZERO).add(it.belop)
-                    ainntektMap[KEY_12MND] = summertAinntekt12Mnd
+                    akkumulerPost(ainntektMap, it, KEY_12MND)
                 }
             }
         }
         return ainntektMap.toMap()
     }
 
-    // Summerer og grupperer ainntekter
-    private fun summerInntekterNy(ainntektListeInn: List<AinntektDto>): Map<String, InntektSumPost> {
-        val ainntektMap = mutableMapOf<String, InntektSumPost>()
-        ainntektListeInn.forEach { ainntekt ->
-            ainntekt.ainntektspostListe.forEach {
-                val aar =
-                    if (it.opptjeningsperiodeFra != null) it.opptjeningsperiodeFra!!.year.toString() else it.utbetalingsperiode!!.substring(0, 4)
-                val inntektSumPost = ainntektMap.getOrDefault(aar, InntektSumPost(BigDecimal.ZERO, mutableListOf()))
-                val sumInntekt = inntektSumPost.sumInntekt
-                val inntektPostListe = inntektSumPost.inntektPostListe
-                inntektPostListe.add(it)
-                ainntektMap[aar] = InntektSumPost(sumInntekt.add(it.belop), inntektPostListe)
-
-                if (innenforAntallMnd(it, 3)) {
-                    val inntektSumPost = ainntektMap.getOrDefault(KEY_3MND, InntektSumPost(BigDecimal.ZERO, mutableListOf()))
-                    val sumInntekt = inntektSumPost.sumInntekt
-                    val inntektPostListe = inntektSumPost.inntektPostListe
-                    inntektPostListe.add(it)
-                    ainntektMap[KEY_3MND] = InntektSumPost(sumInntekt.add(it.belop), inntektPostListe)
-                }
-
-                if (innenforAntallMnd(it, 12)) {
-                    val inntektSumPost = ainntektMap.getOrDefault(KEY_12MND, InntektSumPost(BigDecimal.ZERO, mutableListOf()))
-                    val sumInntekt = inntektSumPost.sumInntekt
-                    val inntektPostListe = inntektSumPost.inntektPostListe
-                    inntektPostListe.add(it)
-                    ainntektMap[KEY_12MND] = InntektSumPost(sumInntekt.add(it.belop), inntektPostListe)
-                }
-            }
-        }
-        return ainntektMap.toMap()
+    // Summerer inntekter og legger til detaljposter til map
+    private fun akkumulerPost(ainntektMap: MutableMap<String, InntektSumPost>, ainntektspost: AinntektspostDto, key: String) {
+        val inntektSumPost = ainntektMap.getOrDefault(key, InntektSumPost(BigDecimal.ZERO, mutableListOf()))
+        val sumInntekt = inntektSumPost.sumInntekt
+        val inntektPostListe = inntektSumPost.inntektPostListe
+        inntektPostListe.add(ainntektspost)
+        ainntektMap[key] = InntektSumPost(sumInntekt.add(ainntektspost.belop), inntektPostListe)
     }
 
     // Sjekker om inntekt-dato er innenfor antall måneder (angitt som parameter)
