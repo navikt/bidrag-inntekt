745e12a3528b9bb0529580ba8dc2f380d1d21fb7
rinnan17 <magnus.rinnan@nav.no>
2023-07-19 11:08:27 +0200

    Consumer for å hente felles kodeverk

--- src/main/kotlin/no/nav/bidrag/inntekt/consumer/kodeverk/KodeverkConsumer.kt
+++ src/main/kotlin/no/nav/bidrag/inntekt/consumer/kodeverk/KodeverkConsumer.kt
@@ -1,19 +1,26 @@
 package no.nav.bidrag.inntekt.consumer.kodeverk
 
+import no.nav.bidrag.commons.CorrelationId
 import no.nav.bidrag.commons.web.HttpHeaderRestTemplate
 import no.nav.bidrag.inntekt.consumer.InntektConsumer
 import no.nav.bidrag.inntekt.consumer.kodeverk.api.GetKodeverkKoderBetydningerResponse
 import no.nav.bidrag.inntekt.consumer.kodeverk.api.HentKodeverkRequest
 import no.nav.bidrag.inntekt.exception.RestResponse
-import no.nav.bidrag.inntekt.exception.tryExchange
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.web.client.RootUriTemplateHandler
 import org.springframework.http.HttpMethod
+import org.springframework.web.client.RestTemplate
 
-private const val KODEVERK_CONTEXT = "/api/v1/kodeverk/Summert skattegrunnlag/koder/betydninger"
+private const val KODEVERK_CONTEXT = "/api/v1/kodeverk"
 
 open class KodeverkConsumer(
-    private val restTemplate: HttpHeaderRestTemplate
+//    private val restTemplate: HttpHeaderRestTemplate
+    private val restTemplate: RestTemplate = HttpHeaderRestTemplate().apply {
+        uriTemplateHandler = RootUriTemplateHandler("$KODEVERK_CONTEXT")
+        addHeaderGenerator("Nav-Call-Id") { CorrelationId.generateTimestamped("bidrag-inntekt").get() }
+        addHeaderGenerator("Nav-Consumer-Id") { "bidrag-inntekt" }
+    }
 ) : InntektConsumer() {
 
     companion object {
@@ -26,16 +33,16 @@ open class KodeverkConsumer(
 
         logger.info("Request kodeverk: $KODEVERK_CONTEXT $request ${initHttpEntityKodeverk(request)} ")
 
-        val restResponse = restTemplate.tryExchange(
-            KODEVERK_CONTEXT,
+        val restResponse = restTemplate.exchange(
+            "/Summert%20skattegrunnlag/koder/betydninger",
             HttpMethod.GET,
             initHttpEntityKodeverk(request),
             GetKodeverkKoderBetydningerResponse::class.java,
             GetKodeverkKoderBetydningerResponse()
         )
 
-        logResponse(logger, restResponse)
+//        logResponse(logger, restResponse)
 
-        return restResponse
+        return RestResponse.Success(restResponse.body ?: GetKodeverkKoderBetydningerResponse())
     }
 }
