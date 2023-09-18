bec1e6c104c474fbf7507fe90bcb162e228e8599
s148719 <jan.kjetil.skar@nav.no>
2023-09-18 09:44:04 +0200

    Fikse WireMockServer for lokal kjøring

--- src/test/kotlin/no/nav/bidrag/inntekt/BidragInntektLocal.kt
+++ src/test/kotlin/no/nav/bidrag/inntekt/BidragInntektLocal.kt
@@ -1,29 +1,62 @@
 package no.nav.bidrag.inntekt
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration
+import com.github.tomakehurst.wiremock.extension.responsetemplating.ResponseTemplateTransformer
+import com.github.tomakehurst.wiremock.junit.WireMockRule
 import no.nav.bidrag.inntekt.BidragInntektLocal.Companion.LOCAL_PROFILE
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.junit.Rule
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.FilterType
 import org.springframework.test.context.ActiveProfiles
+
 
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class])
 @EnableMockOAuth2Server
 @EnableJwtTokenValidation(ignore = ["org.springdoc", "org.springframework"])
 @ActiveProfiles(LOCAL_PROFILE)
-@ComponentScan(excludeFilters = [ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = [BidragInntekt::class, BidragInntektTest::class])])
+@ComponentScan(
+    excludeFilters = [ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        value = [BidragInntekt::class, BidragInntektTest::class]
+    )]
+)
 class BidragInntektLocal {
+
     companion object {
         const val LOCAL_PROFILE = "local"
     }
 }
+
 fun main(args: Array<String>) {
     val profile = if (args.isEmpty()) LOCAL_PROFILE else args[0]
     val app = SpringApplication(BidragInntektLocal::class.java)
     app.setAdditionalProfiles(profile, "lokal-nais-secrets")
     app.run(*args)
+}
+
+@Configuration
+class LocalConfig {
+
+    @Rule
+    var wm = WireMockRule(
+        WireMockConfiguration.options()
+            .extensions(ResponseTemplateTransformer(false))
+    )
+
+    @Bean
+    fun runWiremockServer() {
+        var wms = WireMockServer(1234)
+        wms.start()
+    }
 }
